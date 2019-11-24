a352f33c9f60ba00c8206aa2e01bc06c625afdee
Colin MacKenzie <colin@flyingeinstein.com>
2019-11-24 13:10:54 -0500

    reduced MaskSet to only hold commands sent, requested and completed

--- lib-lss/MaskSet.h
+++ lib-lss/MaskSet.h
@@ -9,19 +9,13 @@ class MaskSet
     // TODO: rename to AsyncTransaction
 {
 public:
-    using Promise = LssPromise<const MaskSet>;
-
-    unsigned long txn;  // current transmission number for this request
+    short busid;
     LssCommands read;
     LssCommands requested;
     LssCommands completed;
 
-    unsigned long timestamp;      // millis when first packet was sent
-    unsigned long expire;         // timestamp when the read request will be considered expired
+    inline MaskSet(short _busid = 0, LssCommands _read = LssInvalid) : busid(_busid), read(_read), requested(0), completed(0) {}
 
-    inline MaskSet(unsigned long _txn = 0, LssCommands _read = LssInvalid) : txn(_txn), read(_read), requested(0), completed(0), timestamp(0), expire(0) {}
-
-    inline bool isActive() const { return txn!=0; }
     inline bool isComplete() const { return read == completed; }
-    inline bool isUnresponsive() const { return completed==0; }
+    //inline bool isUnresponsive() const { return completed==0; }
 };
