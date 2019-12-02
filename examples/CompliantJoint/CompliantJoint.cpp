8391f727d5845052a41d6907fca7aaaa20cd093a
Colin MacKenzie <colin@flyingeinstein.com>
2019-12-02 11:46:35 -0500

    reasonably working compliant joints

--- examples/CompliantJoint/CompliantJoint.cpp
+++ examples/CompliantJoint/CompliantJoint.cpp
@@ -6,22 +6,148 @@
 
 
 CompliantJoint::CompliantJoint(short joint_lssId, std::string _name)
-    : joint(joint_lssId), name(_name), current(0), targetUpdated(false), limp(true)
+    : joint(joint_lssId), name(_name), state(Limp), stateChanged(0), current(0), currentLimit(170), cpr(0), cpr_changed(false) //, targetUpdated(false)
 {
-
+    //fudge.filter(2);
 }
 
-void CompliantJoint::update()
-{
-    if(!limp) {
-        int delta = position.delta();
-        if(current > 300) {
-            int new_pos = 0.5 * (position.current + position.target);
-            moveTo(new_pos);
-        }
+const char* CompliantJoint::stateName(JointState state) {
+    switch(state) {
+        case Disabled: return "Disabled";
+        case Limp: return "Limp";
+        case Holding: return "Holding";
+        case Moving: return "Moving";
+        case PositiveCompliance: return "+Compliance";
+        case NegativeCompliance: return "-Compliance";
+        case ComplianceLimp: return "ComplianceLimp";
+        default:
+            return "?state?";
     }
 }
 
-void CompliantJoint::debugPrint()
-{
+
+void CompliantJoint::enable(bool _enable) {
+    if(isEnabled() != _enable) {
+        transitionTo(_enable ? Limp : Disabled);
+    }
 }
+
+void CompliantJoint::transitionTo(JointState new_state) {
+    if(new_state == state)
+         return; // No change
+
+     prevState = state;
+     stateChanged = millis();
+
+    // we get a chance to do something on the transition
+    auto action = transitionActions.find( StateTransition(state, new_state) );
+    if(action != transitionActions.end()) {
+        // call the action
+        if(action->second) {
+            state = action->second(state, new_state);
+            return;
+        }
+    }
+    state = new_state;
+}
+
+CompliantJoint& CompliantJoint::moveTo(int _position, bool _transition) {
+    position.target(_position);
+    //targetUpdated = true;
+    if(_transition)
+        transitionTo( (position.current().velocity() > 10) ? Moving : Holding );
+    return *this;
+}
+
+void CompliantJoint::update(unsigned long tsnow)
+{
+    if(state <= Limp)
+        return;
+    bool entering = state != prevState;
+    int w;
+
+    if(!isLimp() && tsnow > 1200) {
+        int dx = position.current().velocity();
+        bool pos_polarity = dx > 0;
+        bool neg_polarity = dx < 0;
+
+        currentBias = pos_polarity
+                ? currentPositiveBias.value()
+                : neg_polarity
+                    ? currentNegativeBias.value()
+                    : 0;
+
+        int unbiasedCurrent = current.current() + currentBias;
+        bool limit = unbiasedCurrent > currentLimit;
+        //int amps_velocity = -current.current().velocity();
+
+        unsigned long stateTime = millis() - stateChanged;
+        bool idle = position.current().velocity()==0 && !limit;
+
+
+        switch(state) {
+            case Holding:
+                // check if we are over current limit
+                CPR(3);
+                if(limit) {
+                    if(pos_polarity)
+                        transitionTo(PositiveCompliance);
+                    else if (neg_polarity)
+                        transitionTo(NegativeCompliance);
+                }
+                break;
+
+            case Moving:
+                CPR(3);
+                //if(delta.spread()<2 && delta.average()==0 && !limit)
+                if(idle)
+                        transitionTo(Holding);
+                break;
+
+
+            // seems wierd to split the state, then switch on the same code but we still use discrete polarity state to prevent oscillation
+            case NegativeCompliance:
+            case PositiveCompliance:
+                CPR(1);
+
+                fudge = clamp(current.current().value() / 23, 0, 50);
+                w = (state == NegativeCompliance)
+                    ? -fudge.value()
+                    : fudge.value();
+
+                // just keep adding to target
+                //if (current.current().acceleration() < -25) {
+                //if (current.current().velocity() > 1000) {
+                //if (fudge.velocity() > 1000) {
+                if(fudge > 15 && stateTime > 60) {//} || ((position.current().acceleration() < -50) && stateTime > 200)) {
+                    CPR(1); // reduce filter since we cant update FIR in limp mode
+                    transitionTo(ComplianceLimp);
+                } else {
+                    // half way between target and current (set fudge divider to 12 and limp threshold to 30)
+                    //moveTo( 0.5* ((position.target() + w) + position.current()), false); // todo: try adding measure velocity here
+
+                    //moveTo(position.target() + w + position.current().velocity()/5, false); // todo: try adding measure velocity here
+                    moveTo(position.target() + w, false); // use fudge divider of 20, limp threshold of 15
+                }
+
+                if(idle && stateTime>40)
+                //if(position.current().acceleration() < -10)
+                    transitionTo(Holding);
+
+                break;
+
+            case ComplianceLimp:
+                // keep target tracking current position for when we exit compliance
+                fudge.clear();
+                current.current().clear();
+                moveTo( position.current(), false);
+
+                // if our average is low, and the spread of numbers is narrow, then human is holding us in stable position, retake control
+                if(idle && stateTime > 200)
+                    transitionTo(Holding); // was go to compliance, but what polarity?
+                break;
+        }
+
+    }
+}
+
--- examples/CompliantJoint/CompliantJoints.cpp
+++ examples/CompliantJoint/CompliantJoints.cpp
@@ -10,15 +10,9 @@
 #include "CompliantJoint.h"
 
 
-/// Repeat the update 4 times a second
-#define UPDATE_DELAY			25
 
-int delay = 0;
-
-bool learn_range = false;
-bool learn_model = false;
-
-const int LEARN_MODEL_SPEED[] = {15, 50, 100};
+// allow the use of the Exponential Moving Average Filter
+#define USE_FPC
 
 // a channel represents a bus of servos and is attached to a Arduino Stream
 // (typically a HardwareSerial port)
@@ -32,49 +26,63 @@ CompliantJoint joints[] = {
 };
 
 
-void updateJoints() {
+int delay = 0;
+MovingAverage<int> qtime(2), utime(2);
+
+bool learn_range = false;       // just position read-back to learn servo safety range
+bool learn_model = false;       // beginnings of capturing input/feedback data for training
+
+const int LEARN_MODEL_SPEED[] = {15, 50, 100};
+
+
+void updateJoints(unsigned long now) {
     bool advance_joint = true;
     bool advance_delay = false;
     for(auto& j : joints) {
         if(learn_range) {
-            if(j.position.max == INT_MAX || j.position.current > j.position.max)
-                j.position.max = j.position.current;
-            else if(j.position.min == INT_MIN || j.position.current < j.position.min)
-                j.position.min = j.position.current;
+            if(j.position.max() == INT_MAX || j.position.current() > j.position.max())
+                j.position.max( j.position.current() );
+            else if(j.position.min() == INT_MIN || j.position.current() < j.position.min())
+                j.position.min( j.position.current() );
         }
 
         //joint.debugPrint();
         if(learn_range) {
             // print position extents
             printf("%s[%-4d,%4d]   ",
-                   j.name.c_str(), j.position.min, j.position.max
+                   j.name.c_str(), j.position.min(), j.position.max()
             );
         } else if(learn_model) {
             if(delay >0) {
                 delay--;
                 advance_joint = false;
             } else if(advance_joint) {
-                if(j.position.current + j.velocity.current > j.position.max) {
+                if(j.position.current() + j.velocity.current > j.position.max()) {
                     // at end of joint
-                    j.moveTo(j.position.min);
+                    j.moveTo(j.position.min());
                     //printf("[%s=>%d]\n", j.name.c_str(), j.position.min);
                     advance_delay = advance_joint = true;
                 } else {
                     // normal move
-                    j.moveTo(j.position.current + j.velocity.current);
+                    j.moveTo(j.position.current() + j.velocity.current);
                     advance_joint = false;
                 }
             }
 
             // print current at
             printf("%d,%d,%d,%d, ",
-                   j.position.current, j.position.target, j.measuredVelocity(), j.current
+                   (int)j.position.current(), (int)j.position.target(), j.position.current().velocity(), (int)j.current
             );
         } else {
             // standard mode
-            j.update();
-            printf("%s %-4d° Δ%-4d  %-4dmA   ",
-                   j.name.c_str(), j.position.current, j.position.delta(), j.current
+            printf("%6dms || %s  %-15s", qtime.average()+utime.average(), j.name.c_str(), CompliantJoint::stateName(j.state));
+            j.update(now);
+            //printf("%4d° %4dΔ%-4d   %4dmA   ",
+            //       (int)j.position.current(), j.position.current().velocity(), j.fudge.average(), (int)j.current
+            printf("%4d°  %4dF %4dΔ %4dΔ %4dΔ   %4dmA E%-4d Δ%-4dΔ%-4dΔ%-4d  ",
+                   (int)j.position.current(), j.fudge.value(),
+                   j.position.current().velocity(), j.position.current().acceleration(), j.position.current().jerk(),
+                   j.current.current().value(), j.current.current().value() + j.currentBias, j.current.current().velocity(), j.current.current().acceleration(), j.current.current().jerk()
             );
         }
     }
@@ -88,26 +96,47 @@ void updateJoints() {
             joints[i].velocity.current += LEARN_MODEL_SPEED[i];
         }
     }
-    printf("\n");
+    printf("\r");
 }
 
+#define UPDATE_DELAY			20
 
 int main() {
-    unsigned long now=0;
     int success = 0, failed = 0;
+    bool ready = true;
 
     // put your setup code here, to run once:
     channel.begin("/dev/ttyUSB0", 115200);
 
+#if 0
+    // library doesnt support RESET command yet, so we'll just send as text (and give servos time to complete reset)
+    //channel.transmit("#254RS\r");   usleep(1500000);
+#endif
+
     // disable motion controller on servos, we are going to burst updates so it only gets in the way
     channel.transmit(LynxPacket(254, LssMotionControl, 0));
+#ifdef USE_FPC
     channel.transmit(LynxPacket(254, LssFilterPoleCount, 3));
+#endif
     channel.transmit(LynxPacket(254, LssLEDColor, 3));
-    channel.transmit(LynxPacket(254, LssLimp));
+    //channel.transmit(LynxPacket(254, LssAngularStiffness, 4));
+    //channel.transmit(LynxPacket(254, LssAngularHoldingStiffness, 12));
 
+    /* Some joint setup
+     */
+
+    // set servo safety limits
     joints[0].position.limits(-765, 765);
     joints[1].position.limits(-800, 900);
     joints[2].position.limits(-390, 1000);
+
+    // add a little force bias to account for gravity on the shoulder pitch
+    joints[1].currentNegativeBias = -60;    // only add bias in the 'negative' direction
+
+    // add a little Exponential Moving Average filtering to the current measurements
+    joints[1].current.current().filter(1);
+    joints[2].current.current().filter(1);
+    joints[3].current.current().filter(1);
 
     if(learn_model) {
         joints[0].velocity.current = LEARN_MODEL_SPEED[0];
@@ -124,54 +153,118 @@ int main() {
 
     unsigned long long _quitting_time = millis() + 300000;
     while(millis() < _quitting_time) {
+        ready = false;
+        auto qstart = micros();
+        auto _next_update = qstart + 20000;
 
         channel.send({
-                         LynxPacket(17, LssCurrent|LssQuery),
-                         LynxPacket(17, LssPosition|LssQuery|LssDegrees),
-                         LynxPacket(18, LssCurrent|LssQuery),
-                         LynxPacket(18, LssPosition|LssQuery|LssDegrees),
-                         LynxPacket(19, LssCurrent|LssQuery),
-                         LynxPacket(19, LssPosition|LssQuery|LssDegrees)
+                         LynxPacket(joints[0].joint, LssCurrent|LssQuery),
+                         LynxPacket(joints[0].joint, LssPosition|LssQuery|LssDegrees),
+                         LynxPacket(joints[1].joint, LssCurrent|LssQuery),
+                         LynxPacket(joints[1].joint, LssPosition|LssQuery|LssDegrees),
+                         LynxPacket(joints[2].joint, LssCurrent|LssQuery),
+                         LynxPacket(joints[2].joint, LssPosition|LssQuery|LssDegrees)
                  })
-                .then( [&success](const LssTransaction& tx) {
+                .then( [&success, &ready, qstart](const LssTransaction& tx) {
                     auto packets = tx.packets();
+                    auto now = micros();
+                    qtime.add(now - qstart);
+                    auto ustart = now;
+
                     // update joint measurements
                     for(auto p: packets) {
                         if(p.command & LssQuery && p.hasValue) {
-                            if (p.command & LssCurrent) {
-                                switch (p.id) {
-                                    case 17: joints[0].current = p.value; break;
-                                    case 18: joints[1].current = p.value; break;
-                                    case 19: joints[2].current = p.value; break;
-                                }
-                            } else if (p.command & LssPosition) {
-                                switch (p.id) {
-                                    case 17: joints[0].updatePosition(p.value); break;
-                                    case 18: joints[1].updatePosition(p.value); break;
-                                    case 19: joints[2].updatePosition(p.value); break;
+                            for(auto& j: joints) {
+                                if(j.joint == p.id) {
+                                    if (p.command & LssCurrent) {
+                                        j.current.current(p.value, p.microstamp); break;
+                                    } else if (p.command & LssPosition) {
+                                        j.position.current(p.value, p.microstamp); break;
+                                    }
                                 }
                             }
                         }
                     }
 
                     // update joints
-                    updateJoints();
+                    updateJoints(now/1000);
 
                     std::vector<LynxPacket> updates;
                     for(auto& j: joints) {
-                        if(!j.limp) {
-                            updates.emplace_back(LynxPacket(j.joint, LssPosition|LssAction|LssDegrees, j.position.target));
-                            j.targetUpdated = false;
-                            j.limp = false;
+                        if(!j.isEnabled())
+                            continue;
+
+                        // set color of servo to indicate joint state (Compliance, Limp or Holding/Moving)
+                        switch(j.state) {
+                            case CompliantJoint::NegativeCompliance:
+                            case CompliantJoint::PositiveCompliance:
+                                updates.emplace_back(j.joint, LssLEDColor, LssMagenta);
+                                break;
+
+                            case CompliantJoint::ComplianceLimp:
+                                updates.emplace_back(j.joint, LssLEDColor, LssRed);
+                                break;
+
+                            default:
+                                updates.emplace_back(j.joint, LssLEDColor, LssBlue);
+                                break;
                         }
+
+#ifdef USE_FPC
+                        if(j.cpr_changed) {
+                            //if(j.cpr ==0) {
+                            //    updates.emplace_back(j.joint, LssMotionControl, 1);
+                            //    updates.emplace_back(j.joint, LssMotionControl, 0);
+                            //} else
+                                updates.emplace_back(j.joint, LssFilterPoleCount, j.cpr);
+                                updates.emplace_back(j.joint, LssPosition | LssAction | LssDegrees, j.position.target());
+                            j.cpr_changed = false;
+                            printf("%s CPR %d\n", j.name.c_str(), j.cpr);
+                            // fill the CPR
+                            //for(int i=1; i<j.cpr; i++)
+                            //    updates.emplace_back(j.joint, LssPosition | LssAction | LssDegrees, j.position.target);
+                        }
+#endif
+
+                        switch (j.state) {
+                                break;
+                            case CompliantJoint::PositiveCompliance:
+                            case CompliantJoint::NegativeCompliance:
+                                updates.emplace_back(j.joint, LssPosition | LssAction | LssDegrees, j.position.target());
+
+                            case CompliantJoint::Holding:
+                                updates.insert(updates.end(),
+                                               LynxPacket(j.joint, LssPosition | LssAction | LssDegrees,
+                                                          j.position.target())
+                                                       .currentHaltAndLimp(j.currentLimit)
+                                );
+                                break;
+
+                            case CompliantJoint::Moving:
+                                updates.insert(updates.end(),
+                                               LynxPacket(j.joint, LssPosition | LssAction | LssDegrees,
+                                                          j.position.target())
+                                );
+                                break;
+
+                            case CompliantJoint::ComplianceLimp:updates.emplace_back(j.joint, LssLimp | LssAction);
+                                break;
+                        }
+                        j.position.changed(false);
+
                     }
-                    channel.send(updates.begin(), updates.end());
+                    channel.send(updates.begin(), updates.end()).regardless([&ready, ustart](const LssTransaction& tx2) {
+                        utime.add( micros() - ustart);
+                        ready = true;
+                    });
                 })
-                .otherwise([&failed](const LssTransaction& tx) {
+                .otherwise([&failed, &ready](const LssTransaction& tx) {
                     printf("tx timed out\n");
+                    ready = true;
                 });
 
-        usleep(UPDATE_DELAY * 1000);
+        while(!ready || micros() < _next_update)
+            usleep(500);
     }
 
     printf("sent %d messages (%4.2f msgs/sec)\n", success, success/30.0);
